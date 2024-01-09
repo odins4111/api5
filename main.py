769e86ea5067fce7160650759d0d4d6bdef41402
odins4111 <33553704+odins4111@users.noreply.github.com>
2024-01-09 19:29:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import requests
 
 def main():
     
-    languages = ["JS", "Python", "Java", "Ruby", "PHP", "C++", "CSS", "C#", "C", "GO"]
+    languages = ["JS", "Python", "Java", "Ruby", "PHP", "C++", "C#", "C", "GO", "Kotlin"]
     url = "https://api.hh.ru/vacancies"
     result = {}
     for language in languages:
+        vacancy_info = {}
+        vacancies_processed = 0
+        average_salary = 0
         vacancy_name = "Программист {0}".format(language)
         payload = {
             "area": "1",
@@ -18,21 +21,31 @@ def main():
         response = requests.get(url, params=payload)
         response.raise_for_status()
         response = response.json()
-        result[vacancy_name] = response["found"]
-        if language == "Python":
-            for salary in response["items"]:
-                predict_rub_salary(salary["salary"])
+        #result[vacancy_name] = response["found"]
+        vacancy_info["vacancies_found"] = response["found"]
+        for salary in response["items"]:
+            predict = predict_rub_salary(salary["salary"])
+            #print (predict)
+            if predict != 0:
+                vacancies_processed = vacancies_processed + 1
+                average_salary = average_salary + predict
+        average_salary = average_salary / vacancies_processed
+        vacancy_info["vacancies_processed"] = vacancies_processed
+        vacancy_info["average_salary"] = int(average_salary)
+        result[vacancy_name] = vacancy_info
+    print (result)
 
 
 def predict_rub_salary(vacancy):
+    result = 0
     if vacancy != None:
-        print(vacancy)
         if vacancy["from"] is not None and vacancy["to"] is not None:
-            print ((int(vacancy["from"]) + int(vacancy["to"])) / 2)
+            result =  ((int(vacancy["from"]) + int(vacancy["to"])) / 2)
         elif vacancy["from"] is not None and vacancy["to"] is None:
-            print (int(vacancy["from"]) * 1.2)
+            result = (int(vacancy["from"]) * 1.2)
         elif vacancy["from"] is None and vacancy["to"] is not None:
-            print (int(vacancy["to"]) * 0.8)
+            result = (int(vacancy["to"]) * 0.8)
+    return result
 
 
 if __name__ == '__main__':
